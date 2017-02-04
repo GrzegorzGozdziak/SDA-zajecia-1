65445017939ad1ebeae6d6d926e2e3c6b4a35b4c
unknown <ggozdziak@gmail.com>
2017-02-04 15:13:58 +0100

    Added few methods

--- src/zajecia/czwarte/ZadaniaZeScannerem.java
+++ src/zajecia/czwarte/ZadaniaZeScannerem.java
@@ -11,38 +11,79 @@ public class ZadaniaZeScannerem {
         //checkNumberFromUser();
         //listNumberFromUser();
 
-        compareThreeNumbers();
+        //compareThreeNumbersFromUser();
+        //int[] array = {1, 2, 3, 4, 5, 6, 7};
+        //System.out.println("MIN from array: "+min(array));
+        //System.out.println("MAX from array: "+max(array));
+
+        double avg = avgOfGrades();
+        System.out.println("Your AVG is equal: "+ avg);
+
+
     }
-    public static void compareThreeNumbers() {
+
+    public static double avgOfGrades() {
+        int size = getNumberFromUser("How many grade U want to avg?");
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = getNumberFromUser("Insert "+(i+1)+" grade");
+        }
+        return Zajecia4.avg(array);
+    }
+
+    public static void compareThreeNumbersFromUser() {
         //compare 3 numbesr
         //show max
         // show min
+        String[] message = {"Insert first number", "Insert second number", "Insert third number"};
         System.out.println("Insert three numbers and I will show U which is max and min...");
-        int number1 = getNumberFromUser();
-        int number2 = getNumberFromUser();
-        int number3 = getNumberFromUser();
+        int number1 = getNumberFromUser(message[0]);
+        int number2 = getNumberFromUser(message[1]);
+        int number3 = getNumberFromUser(message[2]);
         System.out.println("Max: "+max(number1,number2, number3));
         System.out.println("Min: "+min(number1,number2, number3));
     }
+    public static int min(int[] array){
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if(array[i] < min){
+                min = array[i];
+            }
+        }
+        return min;
+    }
+    public static int max(int[] array){
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if(array[i] > max){
+                max = array[i];
+            }
+        }
+        return max;
+    }
 
     public static int max(int a, int b, int c){
-        if(a >= b && a >= c){
+        int[] array = {a, b, c};
+        return max(array);
+        /*if(a >= b && a >= c){
             return a;
         } else if(b >= a && b >= c) {
             return b;
         } else {
             return c;
-        }
+        }*/
     }
 
     public static int min(int a, int b, int c){
-        if(a <= b && a <= c){
+        int[] array = {a, b, c};
+        return min(array);
+        /*if(a <= b && a <= c){
             return a;
         } else if(b <= a && b <= c) {
             return b;
         } else {
             return c;
-        }
+        }*/
     }
 
     public static void listNumberFromUser(){
@@ -51,10 +92,19 @@ public class ZadaniaZeScannerem {
     }
 
     public static int getNumberFromUser() {
+        return getNumberFromUser("Please insert some number");
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please insert some number:");
+//        return scanner.nextInt();
+    }
+
+    public static int getNumberFromUser(String message) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please insert some number:");
+        System.out.println(message);
         return scanner.nextInt();
     }
+
+
 
     public static void checkNumberFromUser() {
         Scanner scanner = new Scanner(System.in);
