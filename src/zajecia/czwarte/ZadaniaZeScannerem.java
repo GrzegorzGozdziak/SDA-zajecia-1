6cd1009cf77ec6dacb9fc04e57d41f8729a6570f
unknown <ggozdziak@gmail.com>
2017-02-04 15:22:14 +0100

    Cleaned comments

--- src/zajecia/czwarte/ZadaniaZeScannerem.java
+++ src/zajecia/czwarte/ZadaniaZeScannerem.java
@@ -17,7 +17,7 @@ public class ZadaniaZeScannerem {
         //System.out.println("MAX from array: "+max(array));
 
         double avg = avgOfGrades();
-        System.out.println("Your AVG is equal: "+ avg);
+        System.out.println("Your AVG is equal: " + avg);
 
 
     }
@@ -26,76 +26,58 @@ public class ZadaniaZeScannerem {
         int size = getNumberFromUser("How many grade U want to avg?");
         int[] array = new int[size];
         for (int i = 0; i < array.length; i++) {
-            array[i] = getNumberFromUser("Insert "+(i+1)+" grade");
+            array[i] = getNumberFromUser("Insert " + (i + 1) + " grade");
         }
         return Zajecia4.avg(array);
     }
 
     public static void compareThreeNumbersFromUser() {
-        //compare 3 numbesr
-        //show max
-        // show min
         String[] message = {"Insert first number", "Insert second number", "Insert third number"};
         System.out.println("Insert three numbers and I will show U which is max and min...");
         int number1 = getNumberFromUser(message[0]);
         int number2 = getNumberFromUser(message[1]);
         int number3 = getNumberFromUser(message[2]);
-        System.out.println("Max: "+max(number1,number2, number3));
-        System.out.println("Min: "+min(number1,number2, number3));
+        System.out.println("Max: " + max(number1, number2, number3));
+        System.out.println("Min: " + min(number1, number2, number3));
     }
-    public static int min(int[] array){
+
+    public static int min(int[] array) {
         int min = array[0];
         for (int i = 1; i < array.length; i++) {
-            if(array[i] < min){
+            if (array[i] < min) {
                 min = array[i];
             }
         }
         return min;
     }
-    public static int max(int[] array){
+
+    public static int max(int[] array) {
         int max = array[0];
         for (int i = 1; i < array.length; i++) {
-            if(array[i] > max){
+            if (array[i] > max) {
                 max = array[i];
             }
         }
         return max;
     }
 
-    public static int max(int a, int b, int c){
+    public static int max(int a, int b, int c) {
         int[] array = {a, b, c};
         return max(array);
-        /*if(a >= b && a >= c){
-            return a;
-        } else if(b >= a && b >= c) {
-            return b;
-        } else {
-            return c;
-        }*/
     }
 
-    public static int min(int a, int b, int c){
+    public static int min(int a, int b, int c) {
         int[] array = {a, b, c};
         return min(array);
-        /*if(a <= b && a <= c){
-            return a;
-        } else if(b <= a && b <= c) {
-            return b;
-        } else {
-            return c;
-        }*/
     }
 
-    public static void listNumberFromUser(){
+    public static void listNumberFromUser() {
         int number = getNumberFromUser();
         Zajecia4.printLessThan(number);
     }
 
     public static int getNumberFromUser() {
         return getNumberFromUser("Please insert some number");
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Please insert some number:");
-//        return scanner.nextInt();
     }
 
     public static int getNumberFromUser(String message) {
@@ -105,12 +87,11 @@ public class ZadaniaZeScannerem {
     }
 
 
-
     public static void checkNumberFromUser() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please insert some number:");
         int numberFromUser = getNumberFromUser();
-        if(Zajecia4.isEven(numberFromUser)) {
+        if (Zajecia4.isEven(numberFromUser)) {
             System.out.println("Number is even");
         } else {
             System.out.println("Number is odd");
@@ -125,6 +106,6 @@ public class ZadaniaZeScannerem {
         System.out.println("Please insert Your last name:");
         String lastname = scanner.nextLine();
 
-        System.out.println("U R: "+name+" "+lastname);
+        System.out.println("U R: " + name + " " + lastname);
     }
 }
