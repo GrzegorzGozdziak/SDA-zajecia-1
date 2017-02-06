fd66fbc33cf2c1a95fdfb1c0f4daacf293c1a930
unknown <ggozdziak@gmail.com>
2017-02-06 20:56:49 +0100

    Added new methods using while loop and random object

--- src/zajecia/piate/Zajecia5.java
+++ src/zajecia/piate/Zajecia5.java
@@ -1,10 +1,88 @@
 package zajecia.piate;
 
+import zajecia.czwarte.ZadaniaZeScannerem;
+
+import java.util.Random;
+
 /**
  * Created by RENT on 2017-02-06.
  */
 public class Zajecia5 {
 
+    public static void main(String[] args) {
+//        triangle(3);
+//        square(5);
+//        tree(3);
+//        oneMatrix(4);
+//        evenMatrix(4);
+//        System.out.println(sumOfNumbersFromUser());
+//        System.out.println(arrayFromNumbersFromUser());
+//        printArray(arrayFromNumbersFromUser());
+        Random random = new Random();
+        int stepsToWin = game(random.nextInt(100));
+        System.out.println("U WIN! Ilość kroków: "+stepsToWin);
+    }
+
+    public static int[] arrayFromNumbersFromUser() {
+        int[] tmpArray = new int[100];
+        int size = 0;
+        boolean flag = true;
+        while(flag) {
+            int numberFromUser = ZadaniaZeScannerem.getNumberFromUser("Insert next number");
+            if(numberFromUser != 0) {
+                tmpArray[size] = numberFromUser;
+                size++;
+            } else {
+                flag = false;
+            }
+        }
+        return revriteArray(tmpArray, size);
+    }
+
+    public static int game(int number) {
+        int steps = 0;
+        boolean flag = true;
+        while(flag) {
+            int numberFromUser = ZadaniaZeScannerem.getNumberFromUser("Podaj kolejną liczbę: ");
+            steps++;
+            if(number == numberFromUser) {
+                flag = false;
+            } else if (number < numberFromUser) {
+                System.out.println("Podałeś zbyt dużą liczbę");
+            } else {
+                System.out.println("Podałeś zbyt małą liczbę");
+            }
+        }
+        return steps;
+    }
+
+    public static int[] revriteArray(int[] bigArray, int size) {
+        int [] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = bigArray[i];
+        }
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    public static int sumOfNumbersFromUser() {
+        int sum = 0;
+        boolean flag = true;
+        while(flag) {
+            int numberFromUser = ZadaniaZeScannerem.getNumberFromUser("Insert next number");
+            if(numberFromUser != 0) {
+                sum += numberFromUser;
+            } else {
+                flag = false;
+            }
+        }
+        return sum;
+    }
 
     public static void triangle(int number) {
         for (int i = 0; i < number; i++) {
@@ -73,11 +151,5 @@ public class Zajecia5 {
         }
     }
 
-    public static void main(String[] args) {
-//        triangle(3);
-//        square(5);
-//        tree(3);
-//        oneMatrix(4);
-//        evenMatrix(4);
-    }
+
 }
